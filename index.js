50a0c7d4708455777914b981e61161c2311d9693
Vasu-dhara-12 <vasundharareddy1312@gmail.com>
2026-03-08 18:35:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 80;
 
-app.get('/', (req, res) => res.send('Hello World from Seven Wonders!'));
+app.get('/', (req, res) => res.send('Hello World from woundersbeauty!'));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 '''
